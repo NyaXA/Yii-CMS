a7df87f3151a3575eaa05be5d58e1d6247d53d92
nizsheanez <www.pismeco@gmail.com>
2011-10-15 14:43:58 +0400

    переместил файлы с функциями в libs/functions

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ error_reporting(E_ALL);
 date_default_timezone_set('Europe/Moscow');
 
 require_once($yii);
+require_once(LIBRARY_PATH . 'functions/debug_functions.php');
+require_once(LIBRARY_PATH . 'functions/php_5_3_functions.php');
 
 $session = new CHttpSession;
 $session->open();
@@ -30,19 +32,6 @@ $config = $_SERVER['DOCUMENT_ROOT'] . 'protected/config/' . getenv("APP_ENV") . 
 Yii::createWebApplication($config)->run();
 
 
-function p($data)
-{
-    echo "<pre>";
-    print_r($data);
-    echo "</pre>";
-}
-
-function v($data)
-{
-    echo "<pre>";
-    var_dump($data);
-    echo "</pre>";
-}
 
 ?>
 
