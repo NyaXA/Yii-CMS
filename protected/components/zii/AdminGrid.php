b49bd18d97fca70cf81bfd2ba6a3800a4d6fb9fb
nizsheanez <www.pismeco@gmail.com>
2011-10-21 20:44:50 +0400

    рефакторинг AdminGrid

--- protected/components/zii/AdminGrid.php
+++ protected/components/zii/AdminGrid.php
@@ -5,10 +5,11 @@ class AdminGrid extends CQGridView
     public $template = '{summary}<br/>{pager}<br/>{items}<br/>{pager}';
 
     public $sortable = false;
-    
+
     public function initColumns()
     {
-        if ($this->sortable) {
+        if ($this->sortable)
+        {
             $this->addColumn(array(
                 'class' => 'ext.QGridView.SortableColumn',
                 'header'=> 'Сортировка'
@@ -20,29 +21,29 @@ class AdminGrid extends CQGridView
 
     /**
      * Добавляет колонки перед последней колонкой
+     *
      * @param $configs конфиги для колонок
      */
     public function addColumns($configs)
     {
-        $last_column = $this->columns[count($this->columns) - 1];
-
-        foreach ($configs as $config)
-        {
-            $this->columns[count($this->columns) - 1] = $config;
-        }
-        $this->columns[] = $last_column;
+        $last_index = count($this->columns) - 1;
+        $configs[]  = $this->columns[$last_index];
+        array_splice($this->columns, $last_index, 1, $configs);
     }
 
     /**
      * Добавляет колонку перед последней колонкой
+     *
      * @param $config конфиг колонки
      */
     public function addColumn($config)
     {
-        $last_column = $this->columns[count($this->columns) - 1];
-        $this->columns[count($this->columns) - 1] = $config;
-        $this->columns[] = $last_column;
+        $last_index = count($this->columns) - 1;
+        $configs    = array(
+            $config,
+            $this->columns[$last_index]
+        );
+        array_splice($this->columns, $last_index, 1, $configs);
     }
-
 
 }
