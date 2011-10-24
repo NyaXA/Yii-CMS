1e0f65d44ab64bb41284978cab47cc97c5fee217
nizsheanez <www.pismeco@gmail.com>
2011-10-24 10:40:53 +0400

    теперь в портлете можно использовать функцию requiredFields что бы вернуть массив обязательных параметров

--- protected/components/Portlet.php
+++ protected/components/Portlet.php
@@ -4,6 +4,27 @@ Yii::import('zii.widgets.CPortlet');
 
 class Portlet extends CPortlet 
 {
+    public function init()
+    {
+        $this->_checkRequiredFields();
+        parent::init();
+    }
+
+    private function _checkRequiredFields()
+    {
+        if (method_exists($this, 'requiredFields'))
+        {
+            $fields = $this->getRequiredFields();
+            foreach ($fields as $field)
+            {
+                if ($this->$field === null)
+                {
+                    throw new CException('Параметр '.$this.' является обязательным для портлета '.get_class($this));
+                }
+            }
+        }
+    }
+    
     public function url($route, $params = array(), $ampersand = '&')
     {
         return Yii::app()->controller->url($route, $params = array(), $ampersand = '&');
