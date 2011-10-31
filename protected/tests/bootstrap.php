e9a5cf569be98720cc012bdf158c7460d40f2e78
artem-moscow@yandex.ru <artem@avim.ru>
2011-10-31 21:33:11 +0400

    пофиксил баг в новостях(сортировка по несущ. полю)

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -7,4 +7,13 @@ $config=dirname(__FILE__).'/../config/test.php';
 require_once($yiit);
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
+if (substr($_SERVER['DOCUMENT_ROOT'], -1) != '/')
+{
+    $_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT'].'/';
+}
+
+define("PROTECTED_PATH", $_SERVER["DOCUMENT_ROOT"] . "protected/");
+define("MODULES_PATH", $_SERVER["DOCUMENT_ROOT"] . "protected/modules/");
+define("LIBRARY_PATH", PROTECTED_PATH . "libs/");
+
 Yii::createWebApplication($config);
