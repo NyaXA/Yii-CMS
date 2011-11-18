e0f88b81d9e4d638fbd2e09214ad29df79d252ae
nizsheanez <www.pismeco@gmail.com>
2011-11-18 22:37:58 +0300

    Засунул в один запрос сохранение порядка для файлового мэнеджера

--- protected/modules/fileManager/controllers/FileManagerAdminController.php
+++ protected/modules/fileManager/controllers/FileManagerAdminController.php
@@ -18,7 +18,7 @@ class FileManagerAdminController extends AdminController
     {
         if ($object_id == 0)
         {
-            $object_id = 'tmp_' . Yii::app()->user->id;
+            $object_id = 'tmp_'.Yii::app()->user->id;
         }
 
         $existFiles = FileManager::model()
@@ -27,16 +27,16 @@ class FileManagerAdminController extends AdminController
             ->findAll();
         $this->sendFilesAsJson($existFiles);
     }
-    
+
 
     public function actionUpload($model_id, $object_id, $data_type, $tag)
     {
         if ($object_id == 0)
         {
-            $object_id = 'tmp_' . Yii::app()->user->id;
+            $object_id = 'tmp_'.Yii::app()->user->id;
         }
 
-        $model = new FileManager('insert');
+        $model            = new FileManager('insert');
         $model->object_id = $object_id;
         $model->model_id  = $model_id;
         $model->tag       = $tag;
@@ -74,76 +74,58 @@ class FileManagerAdminController extends AdminController
                 'thumbnail_url' => $file->src,
                 'delete_url'    => $file->deleteUrl,
                 'delete_type'   => "GET",
-                'edit_link'     => UploadHtml::editableLink('Редактировать', $file, 'title', 'fileManagerAdmin/updateAttr', array('class'=>'thumb-edit')),
+                'edit_link'     => UploadHtml::editableLink('Редактировать', $file, 'title', 'fileManagerAdmin/updateAttr', array('class'=> 'thumb-edit')),
                 'id'            => 'File_'.$file->id
             );
         }
 
-//        header('Vary: Accept');
-//        if (isset($_SERVER['HTTP_ACCEPT']) && (strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false))
-//        {
-//            header('Content-type: application/json');
-//        }
-//        else
-//        {
-//            header('Content-type: text/plain');
-//        }
+        //        header('Vary: Accept');
+        //        if (isset($_SERVER['HTTP_ACCEPT']) && (strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false))
+        //        {
+        //            header('Content-type: application/json');
+        //        }
+        //        else
+        //        {
+        //            header('Content-type: text/plain');
+        //        }
 
         echo CJSON::encode($res);
     }
-    
+
 
     public function actionSavePriority()
     {
-        $success = false;
-
         $ids = array_reverse($_POST['File']);
 
         $files = new FileManager('sort');
 
-        $transaction = $files->dbConnection->beginTransaction();
-
-        try
-        {   $i = 0;
-
-            foreach($ids as $id)
-            {
-                $i++;
-                $success = $files->updateAll(array('order'=>$i), 'id='.$id) || $success;
-            }
-            if (!$success)
-            {
-                throw new CException('Some error ☺');
-            }
-
-            $transaction->commit();
-        }
-        catch(Exception $e)
-        {
-            $transaction->rollBack();
-        }
+        $case = SqlHelper::arrToCase('id', array_flip($ids), 't');
+        $c    = Yii::app()->db->commandBuilder->createSqlCommand("UPDATE {$files->tableName()} AS t SET t.order = {$case}");
+        $c->execute();
     }
 
 
     public function actionDelete()
     {
-        $model = $this->loadModel()->delete();
+        $model = $this
+            ->loadModel()
+            ->delete();
     }
 
 
     public function actionUpdateAttr()
     {
         $model = $this->loadModel();
-        
+
         $model->scenario = 'update';
 
         $this->performAjaxValidation($model);
 
         if (isset($_POST[get_class($model)]))
         {
-            $attr = $_GET['attr'];
+            $attr         = $_GET['attr'];
             $model->$attr = $_POST[get_class($model)][$attr];
-            
+
             if ($model->save())
             {
                 echo $model->$_GET['attr'];
@@ -153,19 +135,20 @@ class FileManagerAdminController extends AdminController
 
     public function loadModel()
     {
-        if(isset($_GET['id']))
+        if (isset($_GET['id']))
         {
             $condition = '';
 
-            $model = FileManager::model()->findByPk($_GET['id'], $condition);
+            $model = FileManager::model()
+                ->findByPk($_GET['id'], $condition);
         }
 
-        if($model === null)
+        if ($model === null)
         {
-            throw new CHttpException(404,'The requested page does not exist.');
+            throw new CHttpException(404, 'The requested page does not exist.');
         }
 
-		return $model;
+        return $model;
     }
 
 
@@ -174,16 +157,17 @@ class FileManagerAdminController extends AdminController
         if (isset($_POST['ajax']))
         {
             print_r(CActiveForm::validate($model));
-            Yii::app()->end();
+            Yii::app()
+                ->end();
         }
     }
 
 
     public function actionManage()
     {
-		$model = new FileManager('search');
-		$model->unsetAttributes();
-		if(isset($_GET['FileManager']))
+        $model = new FileManager('search');
+        $model->unsetAttributes();
+        if (isset($_GET['FileManager']))
         {
             $model->attributes = $_GET['FileManager'];
         }
