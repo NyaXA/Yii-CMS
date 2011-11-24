f323aa00e4d8cf9e746ae34a14bf7a794f604bb1
nizsheanez <www.pismeco@gmail.com>
2011-11-24 15:43:31 +0300

    tipInput теперь добавляет класс has-hint, для полей с подсказками

--- js/packages/clientForm/tipInput/tipinput.js
+++ js/packages/clientForm/tipInput/tipinput.js
@@ -1,39 +1,49 @@
 (function($)
 {
+    $.fn.tipClear = function()
+    {
+        var elements = $(this).find('input[type=text], textarea');
+        elements.each(function()
+        {
+            var el = $(this), text = el.data('label');
+            if (el.val() == text)
+            {
+                el.val('');
+            }
+        });
+    };
+
     $.fn.tipInput = function()
     {
-        var elements = $(this).find('input, textarea');
+        var form = $(this), elements = form.find('input[type=text], textarea');
         elements.each(function()
         {
             var el = $(this), text = el.data('label');
 
-            el.val(text)
-                .blur(function()
+            if (el.val().length == 0)
+            {
+                el.val(text).addClass('has-hint');
+            }
+
+            el.blur(
+                function()
                 {
                     if (el.val() == '')
                     {
-                        el.val(text);
+                        el.val(text).addClass('has-hint');
                     }
-                })
-                .focus(function()
+                }).focus(function()
                 {
                     if (el.val() == text)
                     {
-                        el.val('');
+                        el.val('').removeClass('has-hint');
                     }
                 });
-        });
 
-        $(this).submit(function()
+        });
+        form.submit(function()
         {
-            elements.each(function()
-            {
-                var el = $(this), text = el.data('label');
-                if (el.val() == text)
-                {
-                    el.val('');
-                }
-            });
+            form.tipClear();
         });
     };
 })(jQuery);
