9a1d281032538fcdb8a4f56b929c428561fa8125
nizsheanez <www.pismeco@gmail.com>
2012-01-09 16:28:26 +0300

    исправил баг с меню

--- protected/modules/content/models/Menu.php
+++ protected/modules/content/models/Menu.php
@@ -11,111 +11,119 @@ class Menu extends ActiveRecordModel
     }
 
 
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
-
-
-	public function tableName()
-	{
-		return 'menu';
-	}
-
-
-	public function rules()
-	{
-		return array(
-			array('name', 'required'),
-			array('is_visible', 'numerical', 'integerOnly' => true),
-			array('id', 'length', 'max' => 11),
-			array('name', 'length', 'max' => 50),
-            array('name', 'unique', 'className' => 'Menu', 'attributeName' => 'name'),
-            array('id, name, is_visible', 'safe', 'on' => 'search'),
-		);
-	}
-
-
-	public function relations()
-	{
-		return array(
-			'links' => array(
-			    self::HAS_MANY,
-			    'MenuSection',
-			    'menu_id',
-			    'condition' => "lang = '" . Yii::app()->language . "'"
-			),
-		);
-	}
-
-
-	public function search()
-	{
-		$criteria = new CDbCriteria;
-		$criteria->compare('id', $this->id, true);
-		$criteria->compare('name', $this->name, true);
-		$criteria->compare('is_visible', $this->is_visible);
-
-		return new ActiveDataProvider(get_class($this), array(
-			'criteria' => $criteria
-		));
-	}
-
-
-	public function getSections()
-	{
-        $root = MenuSection::model()->roots()->find('menu_id = ' . $this->id);
-
-        $sections = $root->authObject()->children()->findAll();
-
-		foreach ($sections as $i => $section)
-		{
-			if (!$section->is_visible)
-			{
-				unset($sections[$i]);
-			}
-
-			if ($section->page && !$section->page->is_published)
-			{
-				unset($sections[$i]);
-            }
-		}
-
-		return $sections;
-	}
-
-
-    public function getCurrentSection()
+    public static function model($className = __CLASS__)
     {
-        foreach ($this->sections as $section)
-        {
-            if ($section->isActive())
-            {
-                return $section;
-            }
-
-            $childs = $section->children()->findAll();
-            if ($childs)
-            {
-                foreach ($childs as $child)
-                {
-                    if ($child->isActive())
-                    {
-                        return $child;
-                    }
-                }
-            }
-        }
+        return parent::model($className);
     }
 
 
-    public function getPagePath($page_id)
+    public function tableName()
     {
-        $link = MenuSection::model()->findByAttributes(array(
-            'page_id' => $page_id,
-            'menu_id' => $this->id
-        ));
+        return 'menu';
+    }
 
-        return $link->getPath();
+
+    public function rules()
+    {
+        return array(
+            array(
+                'name',
+                'required'
+            ),
+            array(
+                'is_visible',
+                'numerical',
+                'integerOnly' => true
+            ),
+            array(
+                'id',
+                'length',
+                'max' => 11
+            ),
+            array(
+                'name',
+                'length',
+                'max' => 50
+            ),
+            array(
+                'name',
+                'unique',
+                'className'     => 'Menu',
+                'attributeName' => 'name'
+            ),
+            array(
+                'id, name, is_visible',
+                'safe',
+                'on' => 'search'
+            ),
+        );
+    }
+
+
+    public function scopes()
+    {
+        $alias = $this->getTableAlias();
+        return array(
+            'visible' => array('condition' => $alias.'.is_visible = 1')
+        );
+    }
+
+    public function relations()
+    {
+        return array(
+            'links' => array(
+                self::HAS_MANY,
+                'MenuLink',
+                'menu_id',
+                'condition' => "lang = '".Yii::app()->language."'"
+            ),
+        );
+    }
+
+
+    public function search()
+    {
+        $criteria = new CDbCriteria;
+        $criteria->compare('id', $this->id, true);
+        $criteria->compare('name', $this->name, true);
+        $criteria->compare('is_visible', $this->is_visible);
+
+        return new ActiveDataProvider(get_class($this), array(
+            'criteria' => $criteria
+        ));
+    }
+
+
+    public function getSections()
+    {
+        $sections = array();
+
+        $role     = Yii::app()->user->role;
+
+        foreach ($this->visible()->links as $link)
+        {
+            if ($link->parent_id)
+            {
+                continue;
+            }
+
+            if ($link->user_role && ($link->user_role != $role))
+            {
+                continue;
+            }
+            else if ($link->not_user_role && ($link->not_user_role == $role))
+            {
+                continue;
+            }
+
+            if ($link->page && !$link->page->is_published)
+            {
+                continue;
+            }
+
+            $sections[] = $link;
+        }
+
+        return $sections;
     }
 }
