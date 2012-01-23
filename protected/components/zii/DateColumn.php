e77aa834d246435de42663f6d937fd81457edaac
nizsheanez <www.pismeco@gmail.com>
2012-01-23 16:36:18 +0300

    addTimeDiapasonCondition

--- protected/components/zii/DateColumn.php
+++ protected/components/zii/DateColumn.php
@@ -4,15 +4,14 @@ class DateColumn extends CDataColumn
     public $uiDateFormat = 'd.m.yy';
     public $attribute;
 
-
     public function init()
     {
         parent::init();
         $this->uiDateFormat = $this->uiDateFormat ? $this->uiDateFormat : "yy-mm-dd";
         $this->attribute    = $this->attribute ? $this->attribute : $this->name;
 
-        $start        = $this->attribute . '_start';
-        $end          = $this->attribute . '_end';
+        $start        = '_' . $this->attribute . '_start';
+        $end          = '_' . $this->attribute . '_end';
         $_GET[$start] = isset($_GET[$start]) ? $_GET[$start] : 'От';
         $_GET[$end]   = isset($_GET[$end]) ? $_GET[$end] : 'До';
 
